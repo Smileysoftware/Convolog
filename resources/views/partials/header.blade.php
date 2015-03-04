ef27df2977000e8bc2e4eb82e4b74c0cb1ce0032
Lyle Barras <lbarras@palmerhargreaves.com>
2015-03-04 16:58:28 +0000

    Conversations link added to header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -9,6 +9,7 @@
             <li><a href="/auth/login">Login</a></li>
             <li><a href="/auth/register">Register</a></li>
             @else
+            <li><a href="/conversations">Conversations</a></li>
             <li><a href="/auth/logout">Logout</a></li>
             @endif
         </ul>
