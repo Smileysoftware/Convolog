6d835467278a7c97a431e56db7b21bbf55deed81
Lyle Barras <lbarras@palmerhargreaves.com>
2015-03-19 16:05:08 +0000

    Add table sorting to users admin view

--- resources/views/admin/users.blade.php
+++ resources/views/admin/users.blade.php
@@ -4,14 +4,16 @@
 
 <h1>All Users</h1>
 
-<table class="table table-bordered table-striped table-hover">
-    <tr>
-        <th>id</th>
-        <th>Name</th>
-        <th>Email</th>
-        <th>Registered</th>
-        <th>Admin?</th>
-    </tr>
+<table class="table table-bordered table-striped table-hover tablesorter">
+    <thead>
+        <tr>
+            <th>id</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Registered</th>
+            <th>Admin?</th>
+        </tr>
+    </thead>
     @foreach ( $users as $user )
 
         <tr>
